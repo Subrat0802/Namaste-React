a1d764c0e58aac4bfc6abeb7321bba4ee1d6eacb
Subrat Mishra <piyush.subrat8@gmail.com>
2023-04-02 02:18:41 +0530

    incep

---
+++ 1-Inception/app.js
@@ -0,0 +1,53 @@
+    
+    
+    // const heading = React.createElement("h1", {}, "Hello World");
+
+    // ReactDOM.render(heading, document.getElementById("root"));
+
+
+    //https://github.com/Subrat0802/Namaste-React.git
+
+
+
+    //nested elements
+
+    const parent = React.createElement(
+        "div",
+        {is: "parent"},
+        [
+            React.createElement(
+                "div",
+                {id: "child1"},
+                [
+                    React.createElement(
+                    "h1",
+                    {},
+                    "Hello i'm h1 tag"
+                ),
+                    React.createElement(
+                    "h2",
+                    {},
+                    "Hello i'm h2 tag"
+                )
+                ]
+            ),
+            React.createElement(
+                "div",
+                {id: "child2"},
+                [
+                    React.createElement(
+                    "h1",
+                    {},
+                    "Hello i'm h1 tag"
+                ),
+                    React.createElement(
+                    "h2",
+                    {},
+                    "Hello i'm h2 tag"
+                )
+                ]
+            )
+        ]
+    )
+
+    ReactDOM.render(parent, document.getElementById("root"));
